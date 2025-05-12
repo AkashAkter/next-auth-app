86876aeb63e359b39f0535e67c5d92280fa34f9d
AkashAkter <akash31akter@gmail.com>
2025-05-12 22:49:38 +0600

    working on signup backend

---
+++ src/app/api/users/signup/route.ts
@@ -0,0 +1,44 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { connectDB } from "@/dbconfig/dbconfig";
+
+import User from "@/models/userModel";
+import { NextRequest, NextResponse } from "next/server";
+
+import bcrypt from "bcryptjs";
+
+connectDB();
+
+export async function POST(request: NextRequest) {
+  try {
+    const reqBody = await request.json();
+    const { name, email, password } = reqBody;
+
+    console.log(reqBody);
+
+    const user = await User.findOne({ email });
+    if (user) {
+      return NextResponse.json(
+        { error: "User already exists" },
+        { status: 400 }
+      );
+    }
+
+    const salt = await bcrypt.genSalt(10);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const newUser = new User({
+      name,
+      email,
+      password: hashedPassword,
+    });
+    const savedUser = await newUser.save();
+    console.log(savedUser);
+    return NextResponse.json({
+      message: "User created successfully",
+      status: 201,
+      savedUser,
+    });
+  } catch (error: any) {
+    return NextResponse.json({ error: error.message }, { status: 500 });
+  }
+}
